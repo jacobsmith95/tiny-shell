bb0ff2db86d49e1688bcaba597b7a73f2cf2298d
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-03 10:41:49 -0800

    started tiny shell rewrite

---
+++ tinyshell.c
@@ -0,0 +1,26 @@
+#define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+#include <errno.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#ifndef MAX_WORDS
+#define MAX_WORDS 512
+#endif
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+    
+}
